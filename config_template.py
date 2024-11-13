d701f92dfcb295e15243c3e359c16b743586c6f2
10086mea <18366602593@163.com>
2024-11-13 18:41:41 +0800

    更新了配置文件模板

--- config_template.py
+++ config_template.py
@@ -4,12 +4,11 @@
 SMTP_SERVER = "smtp.qq.com"  # SMTP 服务器地址
 SMTP_PORT = 465  # SMTP 服务器端口
 SMTP_USER = "your_email@qq.com"  # 你的邮箱地址
-SMTP_PASSWORD = "your_smtp_password"  # 你的 SMTP 密码，请确保此字段保密
+SMTP_PASSWORD = "your_smtp_password"  # 你的 SMTP 密码
 
 # 应用程序配置
-REMINDER_TIME_THRESHOLD = 96  # 提醒时间阈值（以小时为单位）
 MAX_REMIND_COUNT = 1  # 最多提醒次数
-KAWAII_IMAGE = "path/to/your/image.png"  # 图片文件路径，例: "D:\\path\\to\\image.png"
+KAWAII_IMAGE = "path/to/your/image.png"  # 右下角的二刺螈图片文件路径，例: "D:\\path\\to\\image.png"
 
 # Web 服务的基本 URL
 BASE_URL = "http://123.121.147.7:88/ve"
@@ -17,7 +16,7 @@ BASE_URL = "http://123.121.147.7:88/ve"
 # 其他与 SMTP 相关的配置
 MAIL_SMTP_MODE = "smtp"  # 邮件发送模式，使用 SMTP
 MAIL_SENDMAIL_MODE = "smtp"  # 邮件发送方式，使用 SMTP
-MAIL_FROM_ADDRESS = "your_email_id"  # 发件人地址，例如 "3239799381"
+MAIL_FROM_ADDRESS = "your_email_id"  # 发件人信息，qq邮箱写q号就行
 MAIL_DOMAIN = "qq.com"  # 邮件域名
 MAIL_SMTP_AUTH = True  # 是否启用 SMTP 认证
 MAIL_SMTP_SECURE = "ssl"  # SMTP 加密方式，使用 SSL
--- readme.txt
+++ readme.txt
@@ -1,4 +1,9 @@
+# 介绍页
+https://love.nimisora.icu/archives/371
+# 使用说明
 目前支持两个阈值，一个常规推送一个紧急推送。
-介绍页：https://love.nimisora.icu/archives/371
 需要修改config_template.py文件设置邮箱等内容，并重命名为config.py才能正常使用
 需要修改user_data.csv修改订阅列表
+# 后续更新
+作业通知机要是能自动写作业就好了（
+写完作业再做订阅接口
